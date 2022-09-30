895a6b14e993da89bcc918b5a0803537f3df29d3
SunAndClouds <hani2580449466@gmail.com>
2022-09-30 13:14:40 +0200

    .

---
+++ Project BC.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = [15, 8]
+
+
+# load data
+    # check the shapes
+    # plot some stuff
+
+def main():
+    
+    
+
+
+
+
+if __name__ == '__main__':
+	main()
